2fab3d2f7b83ce1aa1c94ca2b73156f7dc200ef3
diegobtancourt <diegojbb10@gmail.com>
2026-04-12 21:47:46 -0500

    cola.js
    feat: logica de cola (enqueue y dequeue)

--- cola.js
+++ cola.js
@@ -1,1 +1,34 @@
+let cola = [];
+git commit -m "chore: estructura inicial de la cola"
+
+function enqueue(elemento) {
+    cola.push(elemento);
+}
+git commit -m "feat: funcion enqueue implementada"
+
+function dequeue() {
+    if (cola.length === 0) {
+        alert("Cola vacía");
+        return;
+    }
+    cola.shift();
+}
+
+git commit -m "feat: funcion dequeue implementada"
+
+function mostrarCola() {
+    const lista = document.getElementById("colaLista");
+    if (!lista) return;
+
+    lista.innerHTML = "";
+
+    cola.forEach(e => {
+        const li = document.createElement("li");
+        li.textContent = e;
+        lista.appendChild(li);
+    });
+}
+
+git commit -m "feat: renderizado de la cola en la interfaz"
+
 
